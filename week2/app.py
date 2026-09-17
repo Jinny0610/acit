96f2182a3613027882ad8bb1aa08a5ddcd51ae38
leesj06 <lsjcha0610@naver.com>
2026-09-18 08:27:22 +0900

    new files

---
+++ week2/app.py
@@ -0,0 +1,5 @@
+current_year = 2026
+birth_year = input ("Tell me your birth year :")
+birth_year_as_int = int(birth_year)
+
+print(current_year - birth_year)
---
+++ week2/week2_inclass.py
@@ -0,0 +1,11 @@
+# Create a variable named x and store an integer (whole number) inside it
+
+# Create a variable named y and store a string (any characters between single or double quotes) inside it
+
+# Create a variable named z and store a float in it
+
+# Change the value stored in the x variable to a new string
+
+# Change the value stored in the y variable to a new boolean
+
+# Change the value store in the z variable to a different float

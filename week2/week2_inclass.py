101b47af80cab14aa95e1960e948e4a8edf94602
leesj06 <lsjcha0610@naver.com>
2026-09-18 09:52:58 +0900

    new files

--- week2/week2_inclass.py
+++ week2/week2_inclass.py
@@ -1,11 +1,18 @@
 # Create a variable named x and store an integer (whole number) inside it
-
+x = 10
+print(x) 
 # Create a variable named y and store a string (any characters between single or double quotes) inside it
-
+y = "Hello"
+print(y)
 # Create a variable named z and store a float in it
-
+z = 3.14
+print(z)
 # Change the value stored in the x variable to a new string
-
+x = "BCIT"
+print(x)
 # Change the value stored in the y variable to a new boolean
-
+y = True
+print(y)
 # Change the value store in the z variable to a different float
+z = 6.76
+print(z)
